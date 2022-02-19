6fd6883de0b6e253a76c5260a1276cb9a4d576b0
Caio Felipe <devcaiofelipe15@gmail.com>
2022-02-19 11:05:54 -0300

    fix: fix test description

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -30,7 +30,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
-  test('Should return 400 if no email is provided', () => {
+  test('Should return 400 if no password is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
